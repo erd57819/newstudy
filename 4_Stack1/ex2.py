6a2656414a4975389f2ba70738f7e10c0f80156d
박영준 <pyjune@naver.com>
2025-02-13 10:11:21 +0900

    Update ex2.py

--- 4_Stack1/ex2.py
+++ 4_Stack1/ex2.py
@@ -18,6 +18,7 @@ for x in txt:
     elif x == ')':  # 닫는 괄호인 경우
         if top == -1:   # 스택이 비어있으면 (여는 괄호가 없으면 )
             ans = 0
+            break   # for x
         else:           # 여는 괄호 하나 버림
             top -= 1    # pop
 if top != -1:   # 여는 괄호가 남아있으면
